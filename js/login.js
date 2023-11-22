bb75a52f3005eb730b26cc463ab9730a4fcbe472
ashikuraju16 <ashikur.r.raju@gmail.com>
2023-11-22 19:25:57 +0600

    js

---
+++ js/login.js
@@ -0,0 +1,16 @@
+document.getElementById('btn-submit').addEventListener('click',function(){
+
+const emailField = document.getElementById('user-email');
+const email = emailField.value;
+
+const passwordField = document.getElementById('user-password');
+const password = passwordField.value;
+
+if(email === 'ashikur@gmail.com' && password ==="2441139"){
+    console.log ('Dhuke jaw')
+}
+else {
+    console.log ('ber hoye jaaaa')
+}
+
+})
